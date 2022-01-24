a9f9c39506ee81428a549100e25ac64891277efd
simonacristina18 <balaceanusimona18@gmail.com>
2022-01-24 13:40:28 +0200

    Initial commit

---
+++ src/main/java/com/drivingschool/drivingschoolapi/DrivingSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.drivingschool.drivingschoolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrivingSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrivingSchoolApplication.class, args);
+    }
+
+}
